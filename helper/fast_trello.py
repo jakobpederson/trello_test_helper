6503cf59f83cb18cf319a8955d77e6a286e25f15
jakobpederson <jakob.pederson+github@gmail.com>
2018-05-16 17:47:25 -0500

    Enhancement: add cycle generator

--- helper/fast_trello.py
+++ helper/fast_trello.py
@@ -3,11 +3,9 @@ from itertools import cycle
 import os
 from trello import TrelloClient
 from trello.exceptions import Unauthorized
-# from .trello import client_list
-
-
 
 FILE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '.trello'))
+
 
 class FastTrello():
 
@@ -16,12 +14,9 @@ class FastTrello():
         with open(FILE_PATH, 'r') as f:
             for line in f.readlines():
                 self.content.append(line.strip().split(','))
-        self.clients = [TrelloClient(x[0], x[1]) for x in self.content]
-        print(self.clients)
-        # self.user_info = self.get_client()
-        # self.clients = client_list
-        pass
+        self.client_list = self.content
+        self.user_info = self.get_client()
 
-    # def get_client(self):
-    #     for user in cycle(self.client_list):
-    #         yield user_info
+    def get_client(self):
+        for user in cycle(self.client_list):
+            yield TrelloClient(user[0], user[1])
--- tests/test_fast_trello.py
+++ tests/test_fast_trello.py
@@ -5,7 +5,11 @@ from helper.fast_trello import FastTrello
 
 class FastBuilderTests(TestCase):
 
-    def test_x(self):
+    def test_cycle_generator(self):
         builder = FastTrello()
-        # print(builder.user_info.next())
-        self.fail('x')
+        self.assertEqual('a', builder.user_info.__next__().api_key)
+        self.assertEqual('b', builder.user_info.__next__().api_key)
+        self.assertEqual('c', builder.user_info.__next__().api_key)
+        self.assertEqual('a', builder.user_info.__next__().api_key)
+        self.assertEqual('b', builder.user_info.__next__().api_key)
+        self.assertEqual('c', builder.user_info.__next__().api_key)
